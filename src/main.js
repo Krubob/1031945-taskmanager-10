acf4b1dd8142bd39dc90255b29abfaec43a70c57
KruBob <res.19@yandex.ru>
2019-11-24 23:17:13 +0300

    Настраивает eslint

--- src/main.js
+++ src/main.js
@@ -115,13 +115,13 @@ const createBoardTemplate = () => {
 };
 
 const createSortingTemplate = () => {
-return (
-  `<div class="board__filter-list">
+  return (
+    `<div class="board__filter-list">
   <a href="#" class="board__filter">SORT BY DEFAULT</a>
   <a href="#" class="board__filter">SORT BY DATE up</a>
   <a href="#" class="board__filter">SORT BY DATE down</a>
 </div>`
-);
+  );
 };
 
 const createTaskTemplate = () => {
